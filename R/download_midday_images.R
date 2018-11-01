50521c1b438c132ac2101127b81aaafdd6b3a193
bnasr <bijan.s.nasr@gmail.com>
2018-11-01 16:32:15 -0700

    exception handled

--- R/download_midday_images.R
+++ R/download_midday_images.R
@@ -30,15 +30,26 @@ download_midday_images <- function(site, y = year(Sys.Date()), months = 1, days=
   }
 
   if(!dir.exists(download_dir)){
-    dir.create(download_dir)
-    message('directory ', download_dir, ' was created!')
+    stop(download_dir, ' directory does not exist!')
   }
 
   pb <- txtProgressBar(0, n , style = 3)
+
   for(i in 1:n){
-    destfile <- paste0(download_dir, '/', basename(download_list[i]))
-    download.file(download_list[i], destfile = destfile, quiet = TRUE)
+    download_url <- download_list[i]
     setTxtProgressBar(pb, i)
+
+    if(!RCurl::url.exists(download_url)) {
+      warning(download_url, ' does not exist!')
+      next()
+    }
+
+    destfile <- paste0(download_dir, '/', basename(download_url))
+    if(file.exists(destfile)) {
+      warning(destfile, ' was already in ', download_dir)
+      next()
+    }
+    download.file(download_url, destfile = destfile, quiet = TRUE)
   }
 
   download_dir
